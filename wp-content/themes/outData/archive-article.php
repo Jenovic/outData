b31f3093439957ca20104d0e213d24869d29c558
Jenovic Lumu <jenovic@forge.uk>
2020-12-26 13:07:12 +0000

    Add article archive and hero-archive component

---
+++ wp-content/themes/outData/archive-article.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<section class="hero-article">
+	<div class="container">
+			<div class="cols hero-article__content">
+					<div class="col is-12 is-6-md is-6-lg">
+							<h1>Find here what you're looking for</h1>
+					</div>
+					<div class="col is-12 is-6-md is-6-lg flex align-right position-bottom">
+						<?php get_template_part('includes/svgs/article'); ?>
+					</div>
+			</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
