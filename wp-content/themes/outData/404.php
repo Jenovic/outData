c1c9a2a6da863b6df03e7cf1a94004f9bbf1a330
Jenovic Lumu <jenovic@forge.uk>
2021-01-10 20:13:22 +0000

    Add a 404 page

---
+++ wp-content/themes/outData/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<section class="stacked-padding bg-off-white">
+		<div class="container">
+				<div class="cols">
+						<div class="col is-12 load-hidden">
+								<h1 class="xl" style="margin-top: 0;"><?php _e('404', 'outdata'); ?></h1>
+								<p><?php _e("Sorry, the page you're looking for doesn't exist.", 'outdata'); ?></p>
+								<a class="button button--blue" href="/"><span>Back to home</span></a>
+						</div>
+				</div>
+		</div>
+</section>
+
+
+<?php get_footer(); ?>
