ddfa017b5793fffe143507497a9ed7427d727cc6
Jenovic Lumu <jenovic@forge.uk>
2021-02-22 14:47:47 +0000

    Add manifest link

--- wp-content/themes/outData/header.php
+++ wp-content/themes/outData/header.php
@@ -7,9 +7,9 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<!-- <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"> -->
-	<link rel="preload" as="font" href="/wp-content/themes/outdata/src/Fonts/Kollektif.woff" type="font/woff" crossorigin="anonymous">
-  <link rel="preload" as="font" href="/wp-content/themes/outdata/src/Fonts/Kollektif-Bold.woff" type="font/woff" crossorigin="anonymous">
-	<link rel="manifest" href="/manifest.json">
+	<link rel="preload" as="font" href="/wp-content/themes/outData/src/Fonts/Kollektif.woff" type="font/woff" crossorigin="anonymous">
+  <link rel="preload" as="font" href="/wp-content/themes/outData/src/Fonts/Kollektif-Bold.woff" type="font/woff" crossorigin="anonymous">
+	<link rel="manifest" href="/wp-content/themes/outData/manifest.json">
 	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8f61ff">
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="theme-color" content="#ffffff">
