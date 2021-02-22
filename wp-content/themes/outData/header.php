76378835f83b7614758a62984777f93841bcb6ff
Jenovic Lumu <jenovic@forge.uk>
2021-02-22 13:09:46 +0000

    Add google analytic and tag manager codes

--- wp-content/themes/outData/header.php
+++ wp-content/themes/outData/header.php
@@ -13,12 +13,32 @@
 	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8f61ff">
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="theme-color" content="#ffffff">
-	<meta name="Description" content="Dplusm Systems and Tests">
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-N78HTKF');</script>
+	<!-- End Google Tag Manager -->
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-SVKS494GYW"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-SVKS494GYW');
+	</script>
 	<script src="https://unpkg.com/scrollreveal"></script>
 	<title><?php echo wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N78HTKF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <header class="header" id="js-header">
 		<div class="container">
