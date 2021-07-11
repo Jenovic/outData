207bc82c2424903e66f070243a7ee91e11ca586d
Jenovic Lumu <jenovic@forge.uk>
2021-07-11 21:05:35 +0100

    restructure homepage mobile layout

--- wp-content/themes/outData/includes/shared/latest-posts.php
+++ wp-content/themes/outData/includes/shared/latest-posts.php
@@ -17,17 +17,20 @@
 			</div>
 			<?php $post_count = 1; if ( $q->have_posts() ) : while ( $q->have_posts() ) : $q->the_post(); ?>
 				<?php if ( $post_count <= 1 ) : ?>
-					<div class="col is-12 is-8-md is-8-lg">
+					<div class="col is-12 is-8-md is-8-lg desktop-only">
 						<div class="featured-large load-hidden">
 							<?php get_template_part('includes/shared/featured-post-tile'); ?>
 						</div>
 					</div>
 					<div class="featured-small load-hidden">
 				<?php else: ?>
-					<div class="col is-12 is-12-md is-12-lg">
+					<div class="col is-12 is-12-md is-12-lg desktop-only">
 						<?php get_template_part('includes/shared/post-tile-small'); ?>
 					</div>
 				<?php endif; ?>
+				<div class="col is-12 mobile-only">
+					<?php get_template_part('includes/shared/post-tile'); ?>
+				</div>
 			<?php $post_count++; endwhile; endif; wp_reset_postdata(); ?>
 			</div>
 		</div>
